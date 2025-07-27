dfeaf8226802820234d681ba633ab86fb7e94977
nurulikhzan-program <nurulikhzanprogram@gmail.com>
2025-07-27 15:24:20 +0700

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->register()
+            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
